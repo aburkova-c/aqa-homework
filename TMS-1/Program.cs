cd7dc0556e14091470a8175b8ae1419db220de03
Martin R <36480922+gormmmm@users.noreply.github.com>
2026-06-25 10:54:02 +0300

    Lesson 3 work

--- TMS-1/Program.cs
+++ TMS-1/Program.cs
@@ -1,88 +1,15 @@
-﻿Console.WriteLine("Hello this is Rock Paper Scissors");
-Console.WriteLine("Enter your step");
-Console.WriteLine("Enter your step");
+﻿using TMS_1;
 
+var game = new Game(); // object of type (class) Game
 
-var userWon = false;
+game.RoundsToPlay = 5;
+game.Play();
 
-while (userWon == false)
+if (game.UserWon)
 {
-    Console.WriteLine("1 - Rock");
-    Console.WriteLine("2 - Paper");
-    Console.WriteLine("3 - Scissors");
-    Console.WriteLine("0 - Exit");
-    
-    var userInput = Console.ReadLine(); // "5"
-
-    int userChoice;
-
-    // parse user input = string into int 
-    // put result to out result param
-    // return if parse was successful
-
-    if (!int.TryParse(userInput, out userChoice) || !(userChoice >= 0 && userChoice <= 3))
-    {
-        Console.WriteLine("Invalid input");
-        continue;
-    }
-
-    if (userChoice == 0)
-    {
-        return;
-    }
-
-    var random = new Random();
-    var computerChoice = random.Next(1, 4); // generate random number 1-3
-
-    string userChoiceString;
-    switch (userChoice)
-    {
-        case 1:
-            Console.WriteLine("Rock");
-            userChoiceString = "Rock";
-            break;
-        case 2:
-            userChoiceString = "Paper";
-            break;
-        default:
-            userChoiceString = "Scissors";
-            break;
-    }
-
-    Console.WriteLine($"You chose {userChoiceString}");
-
-    string computerChoiceString = computerChoice switch
-    {
-        1 => "Rock",
-        2 => "Paper",
-        _ => "Scissors"
-    };
-
-    Console.WriteLine($"Computer chose {computerChoiceString}");
-
-    if (computerChoice == userChoice)
-    {
-        Console.WriteLine("Draw");
-    }
-    else if (userChoice == 1 && computerChoice == 3 || userChoice == 2 && computerChoice == 1 ||
-             userChoice == 3 && computerChoice == 2)
-    {
-        Console.WriteLine("You win");
-        userWon = true;
-    }
-    else
-    {
-        Console.WriteLine("You lose");
-    }
-
-    // Ctrl + K + D 
+    Console.WriteLine("You won");
 }
-
-
-//data in C# 
-//  int, long 5 10 11
-//  float, double, decimal 56.132
-//  chars - 'a'
-//  strings - "text"
-//  bool - true / false
-//  class
+else
+{
+    Console.WriteLine("You lost");
+}
