bb93292a401d9d7d3cc41773a83030d930b37fc8
burkova <a.burkova@evernode.tech>
2026-06-29 14:45:30 +0200

    Fix lesson-3 as regular directory

---
+++ lesson-3/TMS-1/GameResult.cs
@@ -0,0 +1,18 @@
+﻿namespace TMS_1;
+
+public class GameResult
+{
+    public Move PlayerMove { get; set; }
+    public Move ComputerMove { get; set; }
+    public string ResultText { get; set; } = string.Empty;
+
+    public void Print()
+    {
+        Console.WriteLine($"Player chose: {PlayerMove.Name}");
+        Console.WriteLine($"Computer chose: {ComputerMove.Name}");
+        Console.WriteLine($"Result: {ResultText}");
+    }
+}
+
+// Описать класс результата раунда GameResult со свойствами для хода игрока, хода компьютера и текста результата.
+// Добавить метод Print(), выводящий информацию о раунде.
